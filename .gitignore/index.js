2c9c75f0055e609a60c1c963ea19348b2e8ee1cd
benjamin71 <38401563+benjamin71@users.noreply.github.com>
2018-04-28 15:18:36 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log('DIGI est préte');
+});
+
+bot.login('NDMzOTkyNTE1NjA0MDU0MDE4.DbPVFQ.lyaJgOGSuzztFKb8BD9qyXNLwTs');
